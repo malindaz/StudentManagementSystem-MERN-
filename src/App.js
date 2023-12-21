47795a6b5b7f7c04ca924cd3755101e4f685be44
Malinda Umesh <malindaumesh093@gmail.com>
2023-12-21 13:53:13 +0530

    StudentManagementSystem(MERN)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import AddStudent from './components/AddStudent';
+import AllStudents from './components/AllStudents';
+import Edit from './components/Edit';
+import { Route, BrowserRouter, Routes} from 'react-router-dom';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  //const global = useGlobalContext ();
+  console.log(global);
+  return ( 
+    <div>
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+            <Route path="/" element={<AllStudents/>}></Route>
+            <Route path="/add" element={<AddStudent/>}></Route>
+            <Route path="/update/:id" element={<Edit/>}></Route>
+
+          </Routes>
+      </BrowserRouter>
+  </div>
+
+    
   );
+
 }
 
+
+
+
 export default App;
+
